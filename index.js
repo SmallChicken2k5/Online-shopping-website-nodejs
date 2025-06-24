f04d53261360f87d3967cd5d95ae8605c5ffe259
SmallChicken2k5 <thp.gia@gmail.com>
2025-06-24 16:54:34 +0700

    mongoosedb

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+const database = require('./config/database')
+database.connect();
 const route = require('./routers/client/index.route')
+
+
 
 const app = express();
 const port = process.env.PORT;
