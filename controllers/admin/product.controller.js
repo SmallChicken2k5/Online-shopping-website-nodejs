652506ff3a9ecda1698feca48c6f68d2c5b2602a
SmallChicken2k5 <thp.gia@gmail.com>
2025-06-27 16:40:41 +0700

    form search keyword

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,55 @@
+const product = require('../../models/product.model')
+
 // [GET] /admin/products
-module.exports.product = (req,res) => {
+module.exports.product = async (req,res) => {
+    const find = {
+        deleted: false,
+    };
+    // Button Status
+    const buttonStatus = [
+        {
+            name: 'Tất cả',
+            class:'btn btn-sm btn-outline-success',
+            status: '',
+        },
+        {
+            name: 'Hoạt động',
+            class:'btn btn-sm btn-outline-success ms-1',
+            status: 'active',
+        },
+        {
+            name: 'Dừng hoạt động',
+            class:'btn btn-sm btn-outline-success ms-1',
+            status: 'inactive',
+        }
+    ]
+    buttonStatus.forEach(item => {
+        if (req.query.status === item.status || (item.status === '' && !req.query.status)){
+            item.class += ' active'
+        }
+    })  
+
+    if (req.query.status){
+        find.status = req.query.status;
+    }
+    // End Button Status
+
+    // Form Search Keyword
+    let keyword = '';
+    if (req.query.keyword) {
+        keyword = req.query.keyword;
+        const regex = new RegExp(keyword,'i');
+        find.title = regex;
+    }
+
+
+    // End Form Search Keyword
+
+    const products = await product.find(find);
     res.render('admin/pages/products', {
         title: 'Product Management',
-        message: 'Manage your products here'
+        products: products,
+        buttonStatus: buttonStatus,
+        keyword: keyword
     })
 }
---
+++ public/admin/js/script.js
@@ -0,0 +1,37 @@
+// Start Button status 
+const buttonStatus = document.querySelectorAll('[button-status]');
+if (buttonStatus.length > 0){
+    buttonStatus.forEach( item => {
+        item.addEventListener('click', () => {
+            const status = item.getAttribute('button-status');
+            const url = new URL(window.location.href);
+            if (status){
+                url.searchParams.set('status',status);
+            }
+            else {
+                url.searchParams.delete('status');
+            }
+            window.location.href = url.href;
+        })
+    })
+}
+// End Button status
+
+// Form Search
+
+const formSearch = document.querySelector('#form-search');
+if (formSearch) {
+    formSearch.addEventListener('submit' , e => {
+        e.preventDefault();
+        const url = new URL(window.location.href);
+        const keyword = e.target.elements.keyword.value;
+        if (keyword){
+            url.searchParams.set('keyword', keyword);
+        } else {
+            url.searchParams.delete('keyword');
+        }
+        window.location.href = url.href;
+    })
+}
+
+// End Form Search
