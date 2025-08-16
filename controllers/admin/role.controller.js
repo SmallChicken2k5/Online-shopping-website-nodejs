82d32e85719514e5df5355d3f810015246bf6b39
SmallChicken2k5 <thp.gia@gmail.com>
2025-08-16 17:59:01 +0700

    them edit nhóm phân quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,7 +1,7 @@
 const Roles = require('../../models/roles.model');
 const systemConfig = require('../../config/system');
 // [GET] /admin/roles
-module.exports.index = async (req,res) => {
+module.exports.index = async (req, res) => {
     let find = {
         deleted: false,
     }
@@ -26,3 +26,45 @@ module.exports.createPost = async (req, res) => {
     await record.save();
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
 };
+
+// [GET] /admin/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const find = {
+            _id: id,
+            deleted: false
+        }
+        const record = await Roles.findOne(find);
+        res.render('admin/pages/roles/edit', {
+            title: 'Chỉnh sửa nhóm quyền',
+            record: record,
+        });
+    } catch (error) {
+        req.flash('error', 'Không tìm thấy nhóm quyền');
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
+
+}
+
+// [Patch] /admin/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+    const find = {
+        _id: id,
+        deleted: false
+    }
+    const record = await Roles.findOne(find);
+    if (!record) {
+        req.flash('error', 'Không tìm thấy nhóm quyền');
+        return res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
+
+    // Cập nhật thông tin nhóm quyền
+    record.title = req.body.title;
+    record.description = req.body.description;
+    await record.save();
+
+    req.flash('success', 'Cập nhật nhóm quyền thành công');
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+}
